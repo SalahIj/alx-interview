8b69b0584412882cfc70432e2e60a68239ed7e52
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-05-07 19:42:27 +0000

    Task_0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,38 +1,27 @@
 #!/usr/bin/python3
-"""Module for task 0
-"""
+""" Imported modules """
 
 
 def minOperations(n):
-    """Calculates the fewest number of operations needed to result in
-    exactly n H characters in the file.
-
-    In a text file, there is a single character H. Your text editor can
-    execute only two operations in this file: Copy All and Paste characters
-    in the file.
-
+    """The function definition
     Args:
-        n (int): The number of desired H characters.
-
-    Returns:
-        int: The number of minimal operations needed to get n H characters
-    or 0 if it is impossible to achieve n.
+        n: the input
     """
     if not isinstance(n, int):
         return 0
-    ops_count = 0
-    clipboard = 0
-    done = 1
-    while done < n:
-        if clipboard == 0:
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-        elif n - done > 0 and (n - done) % done == 0:
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-        elif clipboard > 0:
-            done += clipboard
-            ops_count += 1
-    return ops_count
+    a = 0
+    b = 0
+    c = 1
+    while c < n:
+        if b == 0:
+            b = c
+            c += b
+            a += 2
+        elif n - c > 0 and (n - c) % c == 0:
+            b = c
+            c += b
+            a += 2
+        elif b > 0:
+            c += b
+            a += 1
+    return a
